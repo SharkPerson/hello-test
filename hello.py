6b5a819a6c27dd40bfbc0152c93e36466c6fceb4
joshuaw7922 <38376803+joshuaw7922@users.noreply.github.com>
2021-08-31 21:00:24 +1000

    Add for loop

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
 print("Hello world, my name is Josh")
-=======
-print("Hello world my name is Tony")
->>>>>>> 1c3202f3ff3bb8a80f650ed33cfb1e75a0ba74ca
+
+for i in range(10):
+    print(i)
